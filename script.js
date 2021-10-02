893c249d023258fa62cb5c52c91154a849023c21
Eduardo da Silva Dörr <edudorr@hotmail.com>
2021-10-02 20:58:40 -0300

    Att CodePen

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-document.title = "CertifiCard";
-
 function alterarTema() {
 	var dark = document.body.classList.toggle("dark");
 
 	if (dark) {
 		document.getElementById("github").src =
 			"https://i.ibb.co/c12VCd3/github-dark.png";
+		document.getElementById("codepen").src =
+			"https://i.ibb.co/6ZnGybJ/codepen-dark.png";
 	} else {
 		document.getElementById("github").src =
 			"https://i.ibb.co/RpbtwHP/github-light.png";
+		document.getElementById("codepen").src =
+			"https://i.ibb.co/HCXCB04/codepen-light.png";
 	}
 }
